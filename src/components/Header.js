b290eacc67ca516b8f9b460efc22631d13faa970
SusmeetaJ <sush.3996@gmail.com>
2021-05-11 14:59:00 +0530

    Added transition in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,7 @@ const Nav = styled.div`
   padding: 0 36px;
 `;
 const Logo = styled.img`
-  width: 80px;
+  width: 50px;
 `;
 const NavMenu = styled.div`
   display: flex;
@@ -62,6 +62,26 @@ const NavMenu = styled.div`
     span {
       font-size: 13px;
       letter-spacing: 1.42px;
+      position: relative;
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        position: absolute;
+        opacity: 0;
+        transform: scalex(0);
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      }
+    }
+    &:hover {
+      span:after {
+        opacity: 1;
+        transform: scalex(1);
+      }
     }
   }
 `;
